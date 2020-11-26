340487f5a9092d618ea8d15908995d1195c8bd80
Rustique-Uwimpaye <rustique644@gmail.com>
2020-11-26 14:53:09 +0300

    modified models.py

--- instaClone/models.py
+++ instaClone/models.py
@@ -2,14 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
 from cloudinary.models import CloudinaryField
-
-# class Photo(models.Model):
-#     image = CloudinaryField('image')
-
-
-
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
